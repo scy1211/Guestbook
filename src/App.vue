c4dff3dadab3bb8131d659e6d2d603395e5e2f12
yangjaixi <yangjx1943@gmail.com>
2017-03-07 18:22:54 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="router-viwe">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style>
+  @import "assets/style/normalize.css";
+  ul{list-style: none;}
+a{text-decoration: none;}
+  #app {}
+  .router-viwe{padding: 10px;}
+</style>
